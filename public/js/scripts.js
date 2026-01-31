320969548f7b68cd80d667e17014cce81fd05e6c
Łukasz <lukasz.roman.adamski@gmail.com>
2026-02-01 00:32:47 +0100

    Create menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,9 @@
 const overlay = document.querySelector('.intro-scene__flash');
 const music = document.getElementById('background-music');
+const enterButton = document.getElementById('enter');
+const enterSound = document.getElementById('enter-sound');
+
+const fadeLogo = document.getElementById('fade-logo');
 
 function flash()
 {
@@ -13,4 +17,32 @@ function flash()
     }, duration);
 }
 
-flash();
+if(enterButton != null)
+{
+    enterButton.addEventListener('click', async() =>
+    {
+        try
+        {
+            await enterSound.play();
+            enterSound.addEventListener('ended', () => {
+                window.location.href = '/main';
+            });
+        }
+        catch(err)
+        {
+            console.log('We could not play sound ', err);
+        }
+    });
+}
+
+if(overlay != null)
+{
+    flash();
+}
+
+if(fadeLogo != null)
+{
+    setTimeout(() => {
+        fadeLogo.style.opacity = 1;
+    }, 1000);
+}
