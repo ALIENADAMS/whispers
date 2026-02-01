2999ee89d9a8c1335009754cd527ade13fd740f8
Łukasz <lukasz.roman.adamski@gmail.com>
2026-02-01 17:19:30 +0100

    Dodanie wyświetlania okna ui po kliknięciu w element menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,9 +4,21 @@ const enterButton = document.getElementById('enter');
 const enterSound = document.getElementById('enter-sound');
 
 const fadeLogo = document.getElementById('fade-logo');
+
+const uiWindowTitle = document.getElementsByClassName('ui-window__title')[0];
+
+const menuButtons = document.querySelectorAll('.game-window__top-menu-item');
+
+const characterButton = document.getElementById('character');
+
 const backpackButton = document.getElementById('backpack');
 const exitButton = document.getElementById('exit_button');
 const backpack = document.getElementById('backpack_window');
+const backpackMain = document.getElementById('backpack_main');
+
+const notesButton = document.getElementById('notes');
+
+const itemLimit = 42;
 
 function flash()
 {
@@ -50,10 +62,28 @@ if(fadeLogo != null)
     }, 1000);
 }
 
+if(menuButtons != null)
+{
+    menuButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            document.getElementsByClassName('game-window__city')[0].style.opacity = 0.5;
+        });
+    });
+}
+
+if(characterButton != null)
+{
+    characterButton.addEventListener('click', () => {
+        alert('Nowe okno bohatera');
+    });
+}
+
+//Backpack scripts
 if(backpackButton != null)
 {
     backpackButton.addEventListener('click', () => {
         backpack.style.display = 'block';
+        uiWindowTitle.textContent = 'PLECAK';
     });
 }
 
@@ -61,5 +91,23 @@ if(exitButton != null)
 {
     exitButton.addEventListener('click', () => {
         backpack.style.display = 'none';
+        document.getElementsByClassName('game-window__city')[0].style.opacity = 1;
+    });
+}
+
+for(var i = 1; i <= 42; i++)
+{
+    const slot = document.createElement('div');
+    slot.classList.add('ui-window__item');
+    slot.id = 'slot' + i;
+    document.getElementById('backpack_main').appendChild(slot);
+}
+
+//Notes scripts
+if(notesButton != null)
+{
+    notesButton.addEventListener('click', () => {
+        backpack.style.display = 'block';
+        uiWindowTitle.textContent = 'NOTATKI';
     });
 }
